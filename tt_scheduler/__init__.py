3b53d8b438022ac064d2f093a9177fa7c6099fd2
Adithya Mallya <SlaYpoint>
2020-11-13 18:20:54 +0530

    added landing page with login form

--- tt_scheduler/__init__.py
+++ tt_scheduler/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask 
+# from flask_sqlalchemy import SQLAlchemy
+#from flask_bcrypt import Bcrypt 
+# from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '842266388f2c6e4afc87a51854826973'#needs to be hidden during production
+# login_manager = LoginManager(app)
 
 from tt_scheduler import routes
--- tt_scheduler/routes.py
+++ tt_scheduler/routes.py
@@ -1,8 +1,20 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash
 from tt_scheduler import app
+from tt_scheduler.forms import LoginForm
+# from flask_login import login_user, current_user, logout_user, login_required
 
-@app.route('/')
-@app.route('/home') #root page
+@app.route('/',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        #(WIP) just for testing; more stuff to come after dB created; 
+        if form.email.data == "admin@nmit.ac.in" and form.password.data == "password":
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password','danger')
+    return render_template('landing.html', form=form)
+
+@app.route('/home')
 def home():
     return render_template('index.html')
 
@@ -10,14 +22,23 @@ def home():
 def room():
     return render_template('rooms.html')
 
-@app.route('/employee')
+@app.route('/emp')
 def emp():
     return render_template('employee.html')
 
 @app.route('/subjects')
 def subj():
-    return render_template('subjectss.html')
+    return render_template('subjects.html')
 
-@app.route('/classes')
-def sections():
+@app.route('/class')
+def section():
     return render_template('classes.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/logout')
+def logout():
+   return redirect(url_for('login'))
+
