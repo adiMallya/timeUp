1873475bcc4749ff09b8f543eee7de6d6566f4ee
adiMallya <adimallya99@gmail.com>
2020-12-27 09:08:16 +0530

    modified inits

--- tt_scheduler/__init__.py
+++ tt_scheduler/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 
 from config import app_config
 
@@ -19,7 +19,7 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
 
 
-    Bootstrap(app)
+    # Bootstrap(app)
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
